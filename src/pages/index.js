0ace9014082294d8cb985c069be0e36214dcb8d1
nikolamatic11185 <nikolamatic11185@gmail.com>
2020-07-24 13:24:12 +0200

    Added Button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components/"
+import { Container, FeatureImage, Content, ContentCard } from "../components/"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="july 24, 2020"
+          title="Lorem ipsum dolor sit amet"
+          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing"
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
